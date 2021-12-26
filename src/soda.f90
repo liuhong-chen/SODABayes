29494e3b0b507ed8b706ec8590a86f992be6807b
liuhong-chen <67919128+liuhong-chen@users.noreply.github.com>
2021-12-25 19:19:48 -0500

    Update soda.f90

--- src/soda.f90
+++ src/soda.f90
@@ -4,8 +4,7 @@ module soda
     use utilities
     use omp_lib
     
-contains
-    
+contains    
   subroutine choleskyPD(A, U, n, status)
   !choleskyPD computes the Cholesky factorization of a real symmetric
   !positive definite matrix A. Output is an upper triangular matrix U
@@ -56,7 +55,6 @@ contains
   integer, intent(out) :: piv(n)
   integer, intent(out) :: rank, status
   real(real64), intent(inout) :: A(n,n)
-
   real(real64) :: Ajj, DSTOP, EPS, dtemp
   real(real64), allocatable :: work(:)
   integer :: itemp, i, j, jj, p, pvt
@@ -66,14 +64,11 @@ contains
   if( n.LE.0 ) then
     status = -1
     return
-  end if
-  
+  end if  
   do p = 1, n
     piv(p) = p
   end do
-
-  EPS = epsilon(1.0d0)
-  
+  EPS = epsilon(1.0d0) 
   call IDMAX( n, A(1, 1), n+1, pvt, dtemp)
   Ajj = A(pvt, pvt)
   if( Ajj.LE.EPS ) then
@@ -81,13 +76,10 @@ contains
     status = -1
     return
   end if
-
   DSTOP = n*EPS*Ajj
-
   do p = 1, n
     work(p) = 0
   end do
-
   do j = 1, n
     do p = j, n
       if( j.GT.1 ) then
@@ -111,42 +103,36 @@ contains
       CALL dswap( pvt-j-1, A(j, j+1), n, A(j+1, pvt), 1)
       if(pvt<n) then
             CALL dswap( n-pvt, A(j, pvt+1), n, A(pvt, pvt+1), n)
-      end if
-         
+      end if      
       dtemp = work(j)
       work(j) = work(pvt)
       work(pvt) = dtemp
       itemp = piv(pvt)
       piv(pvt) = piv(j)
       piv(j) = itemp
-    end if
-    
+    end if    
     Ajj = sqrt( Ajj )
     A(j,j) = Ajj
-
     if( j.LT.n ) then
       call DGEMV( 'Trans', j-1, n-j, -1.0d0, A( 1, j+1 ), N, A( 1, j ), 1, 1.0d0, A( j, j+1 ), n )
       call DSCAL( n-j, 1.0 / Ajj, A( j, j+1 ), n )
-    end if
-    
+    end if    
   end do
   rank = n
-
   deallocate(work)
   return
 end subroutine choleskyPSD
+
 !subroutines for cholesky decomposition
 !adapted from lapack packages      
 subroutine dswap(n,dx,incx,dy,incy)
        integer incx,incy,n
        real(real64) dx(*), dy(*)
-
        real(real64) DTEMP
        integer I,IX,IY,M,MP1
 
        if (n.LE.0) return
-       if (incx.EQ.1 .AND. incy.EQ.1) then
- 
+       if (incx.EQ.1 .AND. incy.EQ.1) then 
           m = mod(n,3)
           if (m.NE.0) then
              do i = 1,m
@@ -182,16 +168,13 @@ subroutine dswap(n,dx,incx,dy,incy)
           end do
        end if
        return
-
 end subroutine dswap
 
 subroutine IDMAX( n, X, incx, k, r )
 !IDMAX finds the largest component of x, r, and
 !determines the smallest index, k, such that x(k) = r.
-
   real(real64) :: r
   integer :: incx, k, n
-
   real(real64) X(*)
   integer i, ix
 
@@ -199,7 +182,6 @@ subroutine IDMAX( n, X, incx, k, r )
   if( n.LT.1 .OR. incx.LE.0 ) return
   k = 1
   if( n.EQ.1 ) return
-
   if( incx.EQ.1 ) then
     r = X( 1 )
     do i = 2, N
@@ -218,8 +200,7 @@ subroutine IDMAX( n, X, incx, k, r )
           r = X( ix )
         end if
     end do
-  end if
-  
+  end if 
   return
 end subroutine IDMAX
 
@@ -227,8 +208,7 @@ subroutine dgemv(trans, m, n, alpha, A, LDA, x, incx, beta, y, incy)
        real(real64) :: alpha, beta
        integer :: incx, incy, LDA, m, n
        character :: trans
-       real(real64) :: A(LDA,*), x(*), y(*)
-       
+       real(real64) :: A(LDA,*), x(*), y(*)    
        real(real64) :: temp
        integer :: i, info, ix, iy, j, jx, jy, kx, ky, lenx, leny
  
@@ -236,7 +216,6 @@ subroutine dgemv(trans, m, n, alpha, A, LDA, x, incx, beta, y, incy)
        if (.NOT.lsame(trans,'N') .AND. .NOT.lsame(trans,'T') .AND. .NOT.lsame(trans,'C')) return
        if (m.LE.0 .OR. n.LE.0 .OR. LDA.LT.max(1,m) .OR. incx.EQ.0 .OR. incy.EQ.0) return
        if ((alpha.EQ.0.0d0) .AND. (beta.EQ.1.0d0)) return
-
        if (lsame(trans,'N')) then
            lenx = n
            leny = m
@@ -335,16 +314,13 @@ subroutine dgemv(trans, m, n, alpha, A, LDA, x, incx, beta, y, incy)
                end do
            end if
        end if
- 
        return
 end subroutine dgemv
 
 subroutine dscal(n,da,dx,incx)
        real(real64) DA
        integer INCX,N
- 
        real(real64) DX(*)
- 
        integer i,m,mp1,nincx
  
        if (n.LE.0 .OR. incx.LE.0) return
@@ -374,17 +350,14 @@ subroutine dscal(n,da,dx,incx)
 end subroutine DSCAL
 
 logical function lsame(CA,CB)
-
        character ca,cb 
        integer inta,intb,zcode
        lsame = ca .EQ. cb
        
-       if (lsame) return
- 
+       if (lsame) return 
        zcode = ichar('Z')
        inta = ichar(ca)
        intb = ichar(cb)
-
        if (zcode.EQ.90 .OR. zcode.EQ.122) then
            if (inta.GE.97 .AND. inta.LE.122) inta = inta - 32
            if (intb.GE.97 .AND. intb.LE.122) intb = intb - 32
@@ -404,14 +377,13 @@ end function lsame
     
 subroutine power_method ( n, A, lambda, p)
 !subroutine for finding the dominant eigenvalue of matrix A
-
   integer n, p
   real (real64) ::  A(:,:)
   real (real64) lambda, delta
-
   real (real64), allocatable :: y(:), v(:)
   integer :: i, maxit
   real(real64), parameter :: tol = 1.0e-14
+  
   maxit = 1000*n
   allocate(y(n),v(n))
   !initialize
@@ -423,7 +395,6 @@ subroutine power_method ( n, A, lambda, p)
     delta = sum((y-lambda*v)**2) / abs(lambda)
     if ( delta <= tol ) exit
   end do
-
   lambda = abs(lambda)
   deallocate(y,v)
   return
@@ -431,8 +402,8 @@ end subroutine power_method
 
 subroutine setup_soda
   integer k, ios
+  
   if(SODAOFF) return
-
   if(setblocksize) then
       nblocks = ceiling(dble(nloci) / blocksize)
       blockt = nblocks
@@ -462,14 +433,12 @@ subroutine setup_soda
       permvec(k)=k  
      enddo
   endif  
-
   nbatch = ceiling(dble(nind)/(int64*4))
   allocate(genbits(nbatch,nloci))
   call load_geno_bits
   call get_aug_matrix
   naind = naind-naless
-  deallocate(genbits)
-  
+  deallocate(genbits)  
   return
 end subroutine setup_soda  
 
@@ -500,7 +469,6 @@ subroutine get_aug_matrix
     end if 
     n = le - ls + 1
     xxtemp = 0.0d0
-
     !$omp parallel private(jj,kk) 
     !$omp do
     do j=1, n
@@ -545,11 +513,9 @@ subroutine get_aug_matrix
     end if
      naless = min(naless,blocksize-n+nskipped)
   end do 
-
   deallocate(xxtemp)  
   return
 end subroutine get_aug_matrix
-
 
 function xxproduct(jj,kk) result(res)
     integer jj,kk
